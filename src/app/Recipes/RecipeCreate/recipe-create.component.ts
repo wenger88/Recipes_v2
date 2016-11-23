64bd663d15ec1380582ad1ce3684cc650b1207ec
wenger88 <pavlovskigoran88@gmail.com>
2016-11-23 15:44:36 +0100

    header social

--- src/app/Recipes/RecipeCreate/recipe-create.component.ts
+++ src/app/Recipes/RecipeCreate/recipe-create.component.ts
@@ -2,10 +2,11 @@
  * Created by goran.pavlovski on 11/23/2016.
  */
 
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
 import {Router, ActivatedRoute} from "@angular/router";
 import {DataService} from "../../core/services/data.service";
 import {Recipe} from "../../shared/interfaces";
+import {NgForm} from "@angular/forms";
 @Component({
     selector: 'recipe-create',
     template: require('./recipe-create.component.html'),
@@ -20,7 +21,7 @@ export class RecipeCreateComponent implements OnInit{
         steps: []
     }
     postRecipeToServer: string;
-
+    @ViewChild('recipeForm') recipeForm: NgForm;
 
     constructor(private route: ActivatedRoute,
                 private dataService: DataService,
@@ -40,6 +41,49 @@ export class RecipeCreateComponent implements OnInit{
                 () => console.log("Job Done Post !")
             );
         //console.log(this.recipe);
+    }
+    removeDirection(i: number){
+        this.recipe.steps.splice(i,1);
+        this.recipeForm.form.markAsDirty();
+    }
+
+    removeIngredient(i: number){
+        this.recipe.ingredients.splice(i,1);
+        this.recipeForm.form.markAsDirty();
+    }
+
+    addIngredient(name: HTMLInputElement){
+        let ingredient: any = {
+            'name': name.value,
+        }
+
+        if (ingredient.name != ""){
+            console.log(ingredient);
+            this.recipe.ingredients.push(ingredient);
+            name.value = null;
+        }else{
+            console.log('Empty Fields!');
+        }
+
+        console.log(ingredient);
+
+    }
+
+    addDirection(directionName: HTMLInputElement){
+        let direction: any = {
+            'name': directionName.value,
+        }
+
+        if (direction.name != ""){
+            console.log(direction);
+            this.recipe.steps.push(direction);
+            directionName.value = null;
+        }else{
+            console.log('Empty Fields!');
+        }
+
+        console.log(direction);
+
     }
 
 
